5b2117c8e2dcfc3b1b1fe6ebb5765076df44896f
Ricardo <A18107796@idat.edu.pe>
2020-06-03 15:53:29 -0500

    Modificaciones en los moduloes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { PAGES_ROUTES } from './pages.routes';
 
 
+// Modules //
+import { SharedModule } from '../shared/shared.module';
+
+// Components // 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+  ],
   imports: [
-    CommonModule
+    SharedModule,
+    PAGES_ROUTES
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
   ]
 })
 export class PagesModule { }
